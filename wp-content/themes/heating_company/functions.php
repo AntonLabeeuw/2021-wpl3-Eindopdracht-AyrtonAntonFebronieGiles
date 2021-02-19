afa1dc5dcaff8602b793c5ce9fef8958967383ba
AntonLabeeuw <55882296+AntonLabeeuw@users.noreply.github.com>
2021-02-19 13:29:08 +0100

    custom post gemaakt voor merken

--- wp-content/themes/heating_company/functions.php
+++ wp-content/themes/heating_company/functions.php
@@ -47,7 +47,7 @@
 			'label'                 => 'verwarmingsketels',
 			'description'           => 'Soorten verwarmingsketels',
 			'labels'                => $labels,
-			'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields'),
+			'supports'              => array( 'title', 'editor', 'thumbnail'),
 			'hierarchical'          => false,
 			'public'                => true,
 			'show_ui'               => true,
@@ -63,6 +63,58 @@
 			'capability_type'       => 'page',
 		);
 		register_post_type( 'verwarmingsketels', $args );
+    }
+
+	function h_register_merken(){
+        $labels = array(
+			'name'                  => 'merk',
+			'singular_name'         => 'merk',
+			'menu_name'             => 'merken',
+			'name_admin_bar'        => 'merken',
+			'archives'              => 'merk archives',
+			'attributes'            => 'merk attributes',
+			'parent_item_colon'     => 'Parent item:',
+			'all_items'             => 'Alle merken',
+			'add_new_item'          => 'Add new merk',
+			'add_new'               => 'Add merk',
+			'new_item'              => 'New merk',
+			'edit_item'             => 'Edit merk',
+			'update_item'           => 'Update merk',
+			'view_item'             => 'View merk',
+			'view_items'            => 'View merken',
+			'search_items'          => 'Search merk',
+			'not_found'             => 'Not found',
+			'not_found_in_trash'    => 'Not found in trash',
+			'featured_image'        => 'Featured image',
+			'set_featured_image'    => 'Set featured image',
+			'remove_featured_image' => 'Remove featured image',
+			'use_featured_image'    => 'Use as featured image',
+			'insert_into_item'      => 'Insert into item',
+			'uploaded_to_this_item' => 'Uploaded to this item',
+			'items_list'            => 'Items list',
+			'items_list_navigation' => 'Items list navigation',
+			'filter_items_list'     => 'Filter items list',
+		);
+		$args = array(
+			'label'                 => 'merken',
+			'description'           => 'Merken van verwarmingsketels',
+			'labels'                => $labels,
+			'supports'              => array( 'title', 'editor', 'thumbnail',),
+			'hierarchical'          => false,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 5,
+			'menu_icon'				=> 'dashicons-cart',
+			'show_in_admin_bar'     => true,
+			'show_in_nav_menus'     => true,
+			'can_export'            => true,
+			'has_archive'           => true,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
+			'capability_type'       => 'page',
+		);
+		register_post_type( 'merken', $args );
     }
 
     function h_add_custom_box(){
@@ -171,6 +223,7 @@
     // add_action( 'init', 'register_my_menus' );
     add_theme_support( 'post-thumbnails' );
     add_action( 'init', 'h_register_verwarmingsketels', 0 );
+	add_action( 'init', 'h_register_merken', 0 );
 	add_action('add_meta_boxes', 'h_add_custom_box');
     add_action('save_post', 'h_save_postdata');
     add_action('admin_enqueue_scripts', 'p_admin_css_js');

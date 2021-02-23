2241729fe468c47a746581b9470a5aed97764675
AntonLabeeuw <55882296+AntonLabeeuw@users.noreply.github.com>
2021-02-23 11:26:38 +0100

    contact klasse aangemaakt (op basis van tabel die ik aanmaakte in de database)

---
+++ wp-content/themes/heating_company/src/model/contact.php
@@ -0,0 +1,24 @@
+<?php
+
+    class Kamp{
+
+        public $bericht_id;
+        public $voornaam;
+        public $naam;
+        public $email;
+        public $telefoon;
+        public $bericht;
+        public $privacy;
+
+        public function __construct($bericht_id = -1, $voornaam = "", $naam = "", $email = "", $telefoon = "", $bericht = "", $privacy = false){
+            $this->bericht_id = $bericht_id;
+            $this->voornaam = $voornaam;
+            $this->naam = $naam;
+            $this->email = $email;
+            $this->telefoon = $telefoon;
+            $this->bericht = $bericht;
+            $this->privacy = $privacy;
+        }
+    }
+
+?>

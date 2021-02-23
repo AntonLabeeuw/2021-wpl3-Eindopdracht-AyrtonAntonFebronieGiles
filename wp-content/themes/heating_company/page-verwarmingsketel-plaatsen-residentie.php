a9d091f393e3c5c488b4ccda0be5ecf554fac012
AntonLabeeuw <55882296+AntonLabeeuw@users.noreply.github.com>
2021-02-23 09:21:23 +0100

    verwarmingsketel plaatsen pagina's gemaakt en header en footer toegevoegd aan alle pagina's

---
+++ wp-content/themes/heating_company/page-verwarmingsketel-plaatsen-residentie.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+    <main>
+        <div class="container">
+            <h2>Gemeenschappelijke verwarmingsketels</h2>
+            <p>Voor verwarmingsketels in appartementsgebouwen en residenties gelden specifieke regels. The Heating Company is expert in deze materie en kan je verder wegwijs maken in wat kan en mag.</p>
+            <a class="o-button o-button-effect o-button-effect-1" href="#">Neem contact op</a>
+
+            <div class="row">
+                <div class="col l7 m12">
+                    <h2>Waarom een ketel laten plaatsen door The Heating Company?</h2>
+                    <h3>Topmerken aan aantrekkelijke prijzen</h3>
+                    <p>We kiezen samen de beste ketel voor de residentie.</p>
+                    <h3>Snelheid van plaatsing</h3>
+                    <p>We garanderen een vervanging van een ketel binnen de 30 dagen.</p>
+                    <h3>24/7 bereikbaar via ons noodnummer</h3>
+                    <p> +32 56 56 56 16</p>
+                    <h3>Grote vakkennis</h3>
+                    <p>Bel ons de klok rond, ook op zon- en feestdagen.</br>We zijn zeer ervaren in de plaatsing van verwarmingsketelsin residenties.</p>
+                    <h3>Grondige en propere plaatsing</h3>
+                    <p>Na de plaatsing wordt je huis opnieuw in een nette staat achtergelaten.</p>
+                    <a class="o-button o-button-effect o-button-effect-1" href="#">Offerte aanvragen</a>
+                </div>
+                <div class="col l5 m12">
+                    <img src="images/carousel2-groot.jpg" class="c-plaatsen__img" alt="Installatie in een residentie" title="Installatie in een residentie">
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid c-banner-contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m6 l8">
+                        <p class="c-banner-contact-text">Laat ons weten wat je van The Heating Company verwacht</p>
+                    </div>
+                    <div class="col s12 m6 l4">
+                        <a class="o-button o-button-effect o-button-effect-1 right" href="">Neem contact op</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+<?php get_footer(); ?>

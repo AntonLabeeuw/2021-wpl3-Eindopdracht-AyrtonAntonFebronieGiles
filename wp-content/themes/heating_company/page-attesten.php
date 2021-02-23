f289227a0f6bbbffeed14d7d4569c7c13c624187
AntonLabeeuw <55882296+AntonLabeeuw@users.noreply.github.com>
2021-02-23 09:43:52 +0100

    pagina attesten gemaakt in wordpress

---
+++ wp-content/themes/heating_company/page-attesten.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+
+    <main>
+      <div class="container">
+        <h1>Attesten en certificaten</h1>
+        <p>
+          De installateurs van The Heating Company beschikken over de nodige
+          attesten en certificaten om elke opdracht tot een goed einde te
+          brengen.
+        </p>
+        <h2>Behaalde attesten en certificaten:</h2>
+      </div>
+
+      <!-- list -->
+      <div class="container">
+        <!-- voorbeeld van list -->
+        <ul class="c-list">
+          <li class="c-list-item">Onderhouden en afstellen van stookolieinstallaties : TV9189
+          </li>
+          <li class="c-list-item">Onderhouden van gasinstallaties: SGA016
+          </li>
+          <li class="c-list-item">Tanktechnicus : SV00666
+          </li>
+          <li class="c-list-item">Aardgashabilitatie : 02-01661-N
+          </li>
+          <li class="c-list-item">Energiedeskundige : 2007-350-EAP – EPO8489
+          </li>
+        </ul>
+      </div>
+
+      <!-- contact -->
+      <div class="container-fluid c-banner-contact">
+        <div class="container">
+          <div class="row">
+            <div class="col s12 m6 l8">
+              <p class="c-banner-contact-text">
+                Reken op een betrouwbare partner
+              </p>
+            </div>
+            <div class="col s12 m6 l4">
+              <a
+                class="o-button o-button-effect o-button-effect-1 right"
+                href=""
+                >Neem contact op</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <?php get_footer(); ?>

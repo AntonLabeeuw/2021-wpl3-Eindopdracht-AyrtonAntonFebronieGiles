b0e2a3ad8c2b4c1432b55083c9f44e1b9db67ca2
GIles-MIT <giles.van.den.bossche@student.howest.be>
2021-02-23 10:25:56 +0100

    navbar verbeterd

--- wp-content/themes/heating_company/header.php
+++ wp-content/themes/heating_company/header.php
@@ -10,8 +10,20 @@
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-2.2.4.min.js"
         integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <!-- font awesome -->
+     <script src="https://kit.fontawesome.com/e3d5c31b7b.js" crossorigin="anonymous"></script>
+     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+      <!--Import AOS effect-->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="js/effect.js"></script>
+  
+   
    
 </head>
 <body>
@@ -31,40 +43,14 @@
                     href="tel:+32 56 56 56 16"><span>Noodnummer</span></a>
             </div>
         </div>
-        <nav class="z-depth-0">
-            <div class="nav-wrapper bg-color">
-                <div class="container">
-                    <!-- <a href="#" data-activates="mobile-menu" class="button-collapse">
-                        <i class="material-icons nav__hamburger">menu</i>
-                    </a> -->
-                    <!-- desktop menu content -->
-                    <ul class="left hide-on-med-and-down">
-                    <?php
-                                $locations = get_nav_menu_locations();
-                                if(array_key_exists('main-menu', $locations)){
-                                  $idVanNavigatie = $locations['main-menu'];
-                                  $menu_items = wp_get_nav_menu_items($idVanNavigatie);
-                  
-                                  foreach ($menu_items as $item){
-                                    if(home_url($wp->request).'/' == $item->url ){
-                                        echo '<li><a class="nav__item nav__item-active" href="' . $item->url . '">' . $item->title . '</a></li>';
-                                        }else{
-                                        echo '<li><a class="nav__item" href="' . $item->url . '">' . $item->title . '</a></li>';
-                                        echo '<ul id="dropdown1" class="dropdown-content">';
-                                        echo '<li><a class="dropdown-content__item" href="#">' . $item->title . '</a></li>';
-                                        echo '</ul>';
-
-                                        }                                                                              
-                                  }
-                                }else{
-                                  echo "navigatie niet ingesteld op dashboard";
-                                }
-                                
-                    ?>
-
-                    </ul>         
-                    <!-- mobile and tablet menu content -->
-                </div>
-            </div>
-        </nav>
+        <div class="container">
+        <?php
+            wp_nav_menu( array(
+                'menu' => 'main-menu',
+                'theme_location'=>'main-menu',
+                'menu_class' => 'hide-on-med-and-down',
+                'walker' => new wp_materialize_navwalker()
+            ));
+        ?>
+        </div>
     </header>

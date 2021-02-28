7d31e0fbd71537f66587bc91d27a1ce4c81173a7
AntonLabeeuw <55882296+AntonLabeeuw@users.noreply.github.com>
2021-02-28 15:31:37 +0100

    verwerk registratie gemaakt

--- wp-content/themes/heating_company/page-registratie.php
+++ wp-content/themes/heating_company/page-registratie.php
@@ -7,7 +7,7 @@
             </div>
             <section class="c-profielform">
               <div class="container">
-                <form class="o-form">
+                <form class="o-form" action="/registratie-verwerk.php" method="POST">
                   <div class="row">
                     <div
                     class="col s12 m12 l6 xl6 offset-s0 offset-m0 offset-l3 offset-xl3 form-group c-form__group"
@@ -271,10 +271,10 @@
                     >
                       <input
                         type="password"
-                        name="password"
-                        id="password"
+                        name="wachtwoord"
+                        id="wachtwoord"
                         class="form-control c-form__input validate"
-                        placeholder="password"
+                        placeholder="Wachtwoord"
                         minlength="2"
                         required
                       />

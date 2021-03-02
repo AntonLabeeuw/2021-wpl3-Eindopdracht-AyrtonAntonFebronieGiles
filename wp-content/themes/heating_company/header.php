b0a8a2bb9d4a5ca7523e4973e8ed60ee05974873
AyrtonVdD <60926487+AyrtonVdD@users.noreply.github.com>
2021-03-02 09:36:01 +0100

    test bang#

--- wp-content/themes/heating_company/header.php
+++ wp-content/themes/heating_company/header.php
@@ -6,7 +6,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?php echo get_bloginfo('name') ;?></title>
+    <title><?php echo $pagename ;?></title>
     
     
    
